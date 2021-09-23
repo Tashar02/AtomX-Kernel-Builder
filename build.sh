87904a2c9b1ff1238cc515fc31a2529375f44d0d
Divyanshu-Modi <divyan.m05@gmail.com>
2021-09-23 13:52:41 +0530

    Builder: script clean up
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
-# SPDX-License-Identifier: GPL-3.0
-# Copyright (c) 2021, Divyanshu-Modi <divyan.m05@gmail.com>
 #bin/#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0
+# Made by Divyanshu-Modi
+# Revision: 18-09-2021 V1
 
-	COMPILER="$1"
-	USER='OGIndian'
-	HOST="$(uname -n)"
 	VERSION='6.0'
+	COMPILER="$1"
+
+# USER
+	USER='OGIndian'
+	HOST='AtomX-Drone'
+
+# DEVICE CONFIG
 	DEVICENAME='Redmi Note 6 Pro'
 	DEVICE='tulip'
+	DEVICE2=''
+
+# PATH
 	KERNEL_DIR="$HOME/Kernel"
 	ZIP_DIR="$HOME/Repack"
 	AKSH="$ZIP_DIR/anykernel.sh"
+
+# DEFCONFIG
 	DFCF="AtomX-${DEVICE}_defconfig"
 	if [[ ! -f $KERNEL_DIR/arch/arm64/configs/$DFCF ]]; then
 		DFCF="${DEVICE}-perf_defconfig"
@@ -19,13 +29,12 @@
         fi
 	fi
 	CONFIG="$KERNEL_DIR/arch/arm64/configs/$DFCF"
-	mkdir $COMPILER
 
 # Set variables
 	if [[ "$COMPILER" == "CLANG" ]]; then
 		CC='clang'
-		HOSTCC='clang'
-		HOSTCXX='clang++'
+		HOSTCC="$CC"
+		HOSTCXX="$CC++"
 		C_PATH="$HOME/clang"
 		CC_64='aarch64-linux-gnu-'
 		CC_COMPAT='arm-linux-gnueabi-'
@@ -44,9 +53,9 @@
 			CC=$CC                         \
 			LLVM=1                         \
 			LLVM_IAS=1                     \
-			CROSS_COMPILE=$CC_64           \
 			HOSTCC=$HOSTCC                 \
 			HOSTCXX=$HOSTCXX               \
+			CROSS_COMPILE=$CC_64           \
 			PATH=$C_PATH/bin:$PATH         \
 			KBUILD_BUILD_USER=$USER        \
 			KBUILD_BUILD_HOST=$HOST        \
@@ -55,47 +64,56 @@
 			LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH
 	}
 
-	BUILD_START=$(date +"%s")
-
 	CFLAG=$DFCF
 	muke
+
 	source $COMPILER/.config
-	if [[ "CONFIG_LTO_CLANG_FULL=y" ]]; then
-		VARIANT=FULL_LTO
-	elif [[ "CONFIG_LTO_CLANG_THIN=y" ]]; then
-		VARIANT=THIN_LTO
+	if [[ "$CONFIG_LTO_CLANG_THIN" != "y" && "$CONFIG_LTO_CLANG_FULL" == "y" ]]; then
+		VARIANT='FULL_LTO'
+	elif [[ "$CONFIG_LTO_CLANG_THIN" == "y" && "$CONFIG_LTO_CLANG_FULL" == "y" ]]; then
+		VARIANT='THIN_LTO'
 	else
-		VARIANT=NON_LTO
+		VARIANT='NON_LTO'
 	fi
 	telegram-send --format html "Building: <code>$VARIANT</code>"
+
+	BUILD_START=$(date +"%s")
 
 	CFLAG=-j$(nproc)
 	muke
 
+	BUILD_END=$(date +"%s")
+
 	if [[ -f $KERNEL_DIR/$COMPILER/arch/arm64/boot/Image.gz-dtb ]]; then
 		FDEVICE=${DEVICE^^}
 		KNAME=$(echo "$CONFIG_LOCALVERSION" | cut -c 2-)
-		FINAL_ZIP="$KNAME-$FDEVICE-`date +"%H%M"`"
-		cd $ZIP_DIR
+
 		cp $KERNEL_DIR/$COMPILER/arch/arm64/boot/Image.gz-dtb $ZIP_DIR/
+
 		sed -i "s/demo1/$DEVICE/g" $AKSH
 		if [[ "$DEVICE2" ]]; then
 			sed -i "/device.name1/ a device.name2=$DEVICE2" $AKSH
 		fi
+
+		cd $ZIP_DIR
+
+		FINAL_ZIP="$KNAME-$FDEVICE-$VARIANT-`date +"%H%M"`"
 		zip -r9 "$FINAL_ZIP".zip * -x README.md *placeholder zipsigner*
 		java -jar zipsigner* "$FINAL_ZIP.zip" "$FINAL_ZIP-signed.zip"
 		FINAL_ZIP="$FINAL_ZIP-signed.zip"
+
 		telegram-send --file $ZIP_DIR/$FINAL_ZIP
+
 		rm *.zip Image.gz-dtb
+
+		cd $KERNEL_DIR
+
 		sed -i "s/$DEVICE/demo1/g" $AKSH
 		if [[ "$DEVICE2" ]]; then
 			sed -i "/device.name2/d" $AKSH
 		fi
 
-		BUILD_END=$(date +"%s")
 		DIFF=$(($BUILD_END - $BUILD_START))
-
-		cd $KERNEL_DIR
 		COMPILER_NAME="$($C_PATH/bin/$CC --version 2>/dev/null | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
 		telegram-send --disable-web-page-preview --format html "\
