b8ba852100382fcbb289aef65f8360b0b0b07d59
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-17 02:49:23 +0600

    builder: Fallback to `clang` again
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,8 +1,8 @@
 #bin/#!/bin/bash
 
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $HOME/gcc-arm64
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
-#	git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git $HOME/clang
+#	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $HOME/gcc-arm64
+#	git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
+	git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git $HOME/clang
 	git clone --depth=1 https://github.com/Tashar02/AnyKernel3-4.19.git $HOME/Repack
 	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b release-v10 $HOME/Kernel
 
@@ -15,7 +15,7 @@
 	mv build.sh $HOME/Kernel/build.sh
 
 	cd $HOME/Kernel
-#	bash build.sh CLANG
-	bash build.sh GCC
+	bash build.sh CLANG
+#	bash build.sh GCC
 
 	exit
