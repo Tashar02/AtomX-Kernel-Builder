b70d0fc1af1a8c556cec567d69947767faa1f16d
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-06-25 14:47:14 +0600

    builder: Compile `test` branch with EVA GCC Stable and LTO
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
 #bin/#!/bin/bash
 
-#	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $HOME/gcc-arm64
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
+#	git clone --depth=1 https://github.com/KenHV/gcc-arm64 -b master $HOME/gcc-arm64
+	git clone --depth=1 https://github.com/KenHV/gcc-arm -b master $HOME/gcc-arm32
 	git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 $HOME/clang
 	git clone --depth=1 https://github.com/Tashar02/AnyKernel3.git $HOME/Repack
-	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b rebase-2 $HOME/Kernel
+	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test $HOME/Kernel
 
 	pip3 install telegram-send
 
