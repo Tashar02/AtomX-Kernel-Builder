e7e580977f913a7be3fb39ff5596e2fe1b81bf38
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-07-16 22:09:48 +0600

    builder: Change kernel branch to `default-QTI` and comment out GCC
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #	git clone --depth=1 https://github.com/KenHV/gcc-arm64 -b master $HOME/gcc-arm64
-	git clone --depth=1 https://github.com/KenHV/gcc-arm -b master $HOME/gcc-arm32
+#	git clone --depth=1 https://github.com/KenHV/gcc-arm -b master $HOME/gcc-arm32
 	git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 $HOME/clang
 	git clone --depth=1 https://github.com/Tashar02/AnyKernel3.git $HOME/Repack
-	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test $HOME/Kernel
+	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b default-QTI $HOME/Kernel
 
 	pip3 install telegram-send
 
