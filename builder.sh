b41cf6efeeff6bc03f1750395bbf21ee7f4d9e8d
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-02-11 18:39:32 +0600

    github-actions: Revamp!!
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,21 +1,28 @@
-#bin/#!/bin/bash
+#!/usr/bin/env bash
 
-#	git clone --depth=1 https://github.com/KenHV/gcc-arm64 -b master $HOME/gcc-arm64
-	git clone --depth=1 https://github.com/KenHV/gcc-arm -b master $HOME/gcc-arm32
-	git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 $HOME/clang
-	git clone --depth=1 https://github.com/Tashar02/AnyKernel3.git $HOME/Repack
-	git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test $HOME/Kernel
+# Helper function for cloning: gsc = git shallow clone
+gsc() {
+	git clone --depth=1 -q $@
+}
 
-	pip3 install telegram-send
+# Clone GCC
+gsc https://github.com/mvaisakh/gcc-arm64 -b gcc-master $HOME/gcc-arm64
+gsc https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
 
-	mkdir $HOME/.config
-	mv telegram-send.conf $HOME/.config/telegram-send.conf
-	sed -i s/demo1/${BOT_API_KEY}/g $HOME/.config/telegram-send.conf
-	sed -i s/demo2/${CHAT_ID}/g $HOME/.config/telegram-send.conf
-	mv build.sh $HOME/Kernel/build.sh
+# Clone CLANG
+mkdir $HOME/clang && cd $HOME/clang
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+cd ..
 
-	cd $HOME/Kernel
-	bash build.sh CLANG
-#	bash build.sh GCC
+# Clone AnyKernel3
+gsc https://github.com/Tashar02/AnyKernel3.git $HOME/AnyKernel3
 
-	exit
+# Clone Kernel Source
+gsc https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test $HOME/kernel
+
+# Setup Scripts
+mv scarlet.sh $HOME/kernel/scarlet.sh
+cd $HOME/kernel
+
+# Compile the kernel using CLANG
+bash scarlet.sh clang
